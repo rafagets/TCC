7de76fd2bca9c7e73c6d46d56bb7e16df85624b1
rafagets <rafagets2ah@gmail.com>
2016-08-29 00:05:44 -0300

    Implementação layout dos cards de inicio. Ainda esta estatico. Add icone qrcode

--- app/src/main/java/es/esy/rafaelsilva/tcc/home/CabecalhoPost.java
+++ app/src/main/java/es/esy/rafaelsilva/tcc/home/CabecalhoPost.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
@@ -67,9 +68,9 @@ public class CabecalhoPost extends Fragment {
 
         mensagem.show();
 
-        // FORÇA O TECLADO APARECER AO ABRIR O ALERT
-        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+//        // FORÇA O TECLADO APARECER AO ABRIR O ALERT
+//        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+//        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
 
     }
 
--- app/src/main/java/es/esy/rafaelsilva/tcc/home/CorpoHome.java
+++ app/src/main/java/es/esy/rafaelsilva/tcc/home/CorpoHome.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import es.esy.rafaelsilva.tcc.R;
 
@@ -13,9 +15,27 @@ import es.esy.rafaelsilva.tcc.R;
  * Created by Rafael on 25/08/2016.
  */
 public class CorpoHome extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.corpo_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ImageView addOne  = (ImageView) getActivity().findViewById(R.id.imgAddOne);
+        addOne.setOnClickListener(runAddOne());
+    }
+
+    private View.OnClickListener runAddOne() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "add one.", Toast.LENGTH_LONG).show();
+            }
+        };
     }
 }
