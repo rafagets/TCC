8694d50b3d7e06a8d907e18c177db85ad62335cb
Rafa <rafadmlima@yahoo.com.br>
2016-08-31 21:18:27 -0300

    add classes DAO

---
+++ app/src/main/java/es/esy/rafaelsilva/tcc/DAO/DataBase.java
@@ -0,0 +1,27 @@
+package es.esy.rafaelsilva.tcc.DAO;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Rafa on 31/08/2016.
+ */
+public class DataBase extends SQLiteOpenHelper {
+
+    public DataBase(Context context){
+        super(context, "APP_INI", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //criar os scripts de criacao do banco
+        db.execSQL(ScriptSQL.getCreateDataBase());
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+    //manutencao no banco
+    }
+}
---
+++ app/src/main/java/es/esy/rafaelsilva/tcc/DAO/ScriptSQL.java
@@ -0,0 +1,20 @@
+package es.esy.rafaelsilva.tcc.DAO;
+
+/**
+ * Created by Rafa on 31/08/2016.
+ */
+public class ScriptSQL {
+
+    public static String getCreateDataBase(){
+        StringBuilder sqlBuilder = new StringBuilder();
+        sqlBuilder.append("CREATE TABLE IF NOT EXISTS USUARIO ( " );
+        sqlBuilder.append("_id            INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
+        sqlBuilder.append("NOME           VARCHAR(40), ");
+        sqlBuilder.append("EMAIL          VARCHAR (255), ");
+        sqlBuilder.append("SENHA          VARCHAR(300) ");
+        sqlBuilder.append("); ");
+
+        return sqlBuilder.toString();
+
+    }
+}
